b5e6d7c61eea8ec12c2ab2108b9c1b71ed33f2f7
coding-to-music <connors.tom@gmail.com>
2022-12-08 06:46:12 +0000

    Build

--- config/express.js
+++ config/express.js
@@ -56,7 +56,7 @@ module.exports = function(app, passport) {
 
   app.use(
     cors({
-      origin: ['http://localhost:3000', 'https://reboil-demo.herokuapp.com'],
+      origin: ['http://localhost:3000', 'https://reboil-demo.herokuapp.com', 'https://node-express-mongoose-demo.vercel.app'],
       optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
       credentials: true
     })
